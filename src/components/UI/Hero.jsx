4d3088105d1c6fb68ed6ae72dcc9e804d5442bfe
bennkemboi <benkemboi23@gmail.com>
2023-06-18 18:29:25 +0300

    add first react app

---
+++ src/components/UI/Hero.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Hero = () => {
+    return <section>
+        <div className="container">
+            <div className="hero_wrapper">
+                <div className="hero_content">
+
+                </div>
+                <div className="hero_content"></div>
+       {/* --- Hero image -----}'''
+       
+                <div className="hero_img"></div>
+            </div>
+        </div>
+    </section> 
+       
+    
+}
+
+export default Hero
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import '../styles/header.css';
+
+const nav__links = [
+    {
+        Path: '#',
+        display: 'Home'
+    },
+    {
+        Path: '#',
+        display: 'Schedule'
+    },
+    {
+        Path: '#',
+        display: 'Classes'
+    },
+    {
+        Path: '#',
+        display: 'Pricing'
+    },
+]
+
+const Header = () => {
+    return (
+    <header className='header'>
+        <div className="container">
+            <div className="nav__wrapper">
+                     {/* LOGO */}
+                <div className="logo">
+                    <div className="logo__img"> <img src="" alt='logo' /></div>
+                    <h2>BodyFiti</h2>
+                </div>
+            {/* NAVIGATION MENU */} 
+            <div className="navigation">
+                <ul className='menu'>
+
+                {
+                    nav__links.map(item => (
+                        <li className='nav_item'><a href={item.path}>
+                            {item.display}</a></li>
+                    ))
+                }
+                </ul>
+            </div>
+            {/* NAVIGATION RIGHT */}
+            <div className="nav_right">
+                <button className='register_btn'>Register</button>
+                <span className='mobile_menu'></span>
+            </div>
+            </div>
+        </div>
+    </header>
+    )
+};
+
+export default Header;
